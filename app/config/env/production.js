cc850635b018bfc4e350ba899b982772d66eb502
frank <18652171101@163.com>
2017-03-10 11:07:30 +0800

    keep foder

--- app/config/env/production.js
+++ app/config/env/production.js
@@ -2,4 +2,16 @@
  * Created by frank on 2016/11/1.
  */
 
-module.exports = {}
+module.exports = {
+  host: 'localhost',
+  port: process.env.NODE_PORT || 3000,
+  db: {
+    database: 'kd-sound',
+    username: 'mooc1988',
+    password: 'Mooc1988',
+    config: {
+      host: 'rdsfom58rh8q9mrl8885.pg.rds.aliyuncs.com',
+      dialect: 'postgres'
+    }
+  }
+}
