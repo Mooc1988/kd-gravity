7a2e577a0d1982c03236a5cb3a0b8b75baeff766
frank <18652171101@163.com>
2017-03-12 13:21:29 +0800

    代码格式化

--- app/config/start.js
+++ app/config/start.js
@@ -6,23 +6,25 @@
 const chalk = require('chalk')
 
 module.exports = function start () {
-    this.init(err => {
-        if (err) {
-            return this.stop(err)
-        }
-        let { config, server } = this
-        server.listen(config.port, () => {
-            const server = `http://${config.host}:${config.port}`
-            console.log('--')
-            console.log(chalk.green(`App name:          ${config.name}`))
-            console.log(chalk.green(`App version:       ${config.version}`))
-            console.log()
-            console.log(chalk.green(`Environment:       ${process.env.NODE_ENV}`))
-            console.log(chalk.green(`Server:            ${server}`))
-            console.log(chalk.green(`Database:          ${config.db.uri}`))
-            console.log('--')
-            this.emit('start')
-        })
+  this.init(err => {
+    if (err) {
+      return this.stop(err)
+    }
+    let {config, server} = this
+    server.listen(config.port, () => {
+      const server = `http://${config.host}:${config.port}`
+      console.log('--')
+      console.log(chalk.green(`App name:          ${config.name}`))
+      console.log(chalk.green(`App version:       ${config.version}`))
+      console.log()
+      console.log(chalk.green(`Environment:       ${process.env.NODE_ENV}`))
+      console.log(chalk.green(`Server:            ${server}`))
+      console.log(chalk.green(`Database:          ${config.db.database}`))
+      console.log('--')
+      this.emit('start')
     })
+  })
 }
+
+
 
